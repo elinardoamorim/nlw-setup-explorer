506ecac3fe5b2d6c3c44b513e175d5c47c228f89
Elinardo Amorim <elinardoslva@gmail.com>
2023-01-22 14:08:20 -0300

    Ajustes de layout, e feito função para marcar os dias na qual foram feito alguma atividade.

---
+++ script.js
@@ -0,0 +1,14 @@
+const form =  document.querySelector('#form-habits')
+const nlwSetup = new NLWSetup(form)
+
+const data = {
+  run: ['01-01', '01-03', '01-04', '01-06'],
+  water: ['01-01', '01-02', '01-03', '01-04', '01-05', '01-06'],
+  food: ['01-01', '01-02', '01-03', '01-04', '01-05', '01-06'],
+  journal: ['01-02', '01-03', '01-05', '01-06'],
+  takePills: ['01-01', '01-02', '01-04', '01-05']
+}
+
+nlwSetup.setData(data)
+
+nlwSetup.load()
